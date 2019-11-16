107bba647e92de198ed85fa1a1cf394f6411bc1d
naasyakoot <naasyakoot@outlook.com>
2019-11-16 15:41:55 +0600

    takes history snap

--- history.php
+++ history.php
@@ -1,1 +1,37 @@
 <?php
+<?php
+
+    class Person {
+
+        private $name;
+
+        private $age;
+
+        public function __constructor ($name) {
+
+            $this -> name = $name;
+        }
+
+        public function getAge (  ) {
+
+            return $this -> age * 365;
+        }
+
+        public function setAge ( $age ) {
+
+            if ( $age < 10 ) {
+
+                throw new Exception ( 'Person is not old Enough' );
+            }
+            $this -> age = $age;
+        }
+    }
+
+$jhon = new Person ( 'Jhon Doe' );
+
+$jhon -> setAge(30);
+
+$jhon -> age = 9;
+
+var_dump ( $jhon -> getAge() );
+?>
