71d17f308604019eb8d5ef799b2c7273d3abdd3a
naasyakoot <naasyakoot@outlook.com>
2019-11-16 15:36:17 +0600

    private mechanism tested

--- history.php
+++ history.php
@@ -1,1 +1,27 @@
 <?php
+
+// Switch is reserved
+class LightSwitch {
+
+        public function on () {
+
+        }
+
+        public function off () {
+
+
+        }
+
+        private function connect () {
+
+
+        }
+
+        public function activate () {
+
+
+        }
+    }
+$switch = new LightSwitch();
+
+$switch -> connect();
--- index.php
+++ index.php
@@ -12,7 +12,7 @@ class LightSwitch {
 
         }
 
-        public function connect () {
+        private function connect () {
 
 
         }
