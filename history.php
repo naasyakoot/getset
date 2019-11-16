492fc0c90b3ae782cd90e182732708bc1ef79320
naasyakoot <naasyakoot@outlook.com>
2019-11-16 15:37:54 +0600

    with protected

--- history.php
+++ history.php
@@ -25,3 +25,34 @@ class LightSwitch {
 $switch = new LightSwitch();
 
 $switch -> connect();
+
+?>
+
+<?php
+
+// Switch is reserved
+class LightSwitch {
+
+        public function on () {
+
+        }
+
+        public function off () {
+
+
+        }
+
+        protected function connect () {
+
+
+        }
+
+        public function activate () {
+
+
+        }
+    }
+$switch = new LightSwitch();
+
+$switch -> connect();
+?>
